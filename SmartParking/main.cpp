58849566bf35497bd75eb6f631565f9495993b27
bianorz <10.bianor@gmail.com>
2017-10-05 14:48:31 -0300

    add linux time

--- SmartParking/main.cpp
+++ SmartParking/main.cpp
@@ -8,11 +8,23 @@
 #include "useful_dip_lib.h"
 #include <fstream>
 #include <iostream>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
+#include <ctime>
+#include <stack>
 using namespace cv;
 using namespace std;
+
+std::stack<clock_t> tictoc_stack;
+
+void tic() {
+	tictoc_stack.push(clock());
+}
+
+void toc() {
+	std::cout << "Time elapsed: "
+			<< ((double) (clock() - tictoc_stack.top())) / CLOCKS_PER_SEC
+			<< " sec" << std::endl;
+	tictoc_stack.pop();
+}
 
 void Whtml(float a) {
 
@@ -20,13 +32,14 @@ void Whtml(float a) {
 	myfile.open("vagas.html");
 
 	myfile << "<!DOCTYPE html>\n<html>\n<head>\n<h1>Smart Parking</h1>\n";
-	myfile << "<meta http-equiv=\"refresh\" content=\"1\" />\n</head>\n<body>\n";
+	myfile
+			<< "<meta http-equiv=\"refresh\" content=\"1\" />\n</head>\n<body>\n";
 	myfile << "vagas livres =  " << a << "\n";
 	myfile << "<h2>Mapa de Vagas</h2>\n";
-	myfile << "<img src=\"alpha.png\" alt=\"\" style=\"width:304px;height:228px;\">\n";
+	myfile
+			<< "<img src=\"alpha.png\" alt=\"\" style=\"width:304px;height:228px;\">\n";
 	myfile << "</body>\n</html>";
 	myfile.close();
-
 
 }
 
@@ -64,24 +77,27 @@ int main() {
 		histograma_mapa_h[i] = histCalc(channels[0], 180);
 	}
 
-
 	/*
 	 If para verificação se existe camera conectada ao sistema
 	 */
-    VideoCapture capture; //
+	VideoCapture capture; //
 
-    capture.open("http://10.7.161.98:10088/?action=stream");
-    if (!capture.isOpened())  // if not success, exit program
-    {
-        cout << "Cannot open the video cam" << endl;
-        return -1;
-    }
+	capture.open("http://10.7.161.98:10088/?action=stream");
+	if (!capture.isOpened())  // if not success, exit program
+	{
+		cout << "Cannot open the video cam" << endl;
+		return -1;
+	}
 	//********************LOOP INFINITO PARA PROCESSAMENTO DA IMAGEM**********************
 	int ocupacao = 0;
-	int totalVagas =0;
+	int totalVagas = 0;
 	int aux = 0;
 	int atual = 0;
-    while (1) {
+	while (1) {
+		if (aux == 0) {
+
+			tic();
+		}
 		capture.read(src);
 		src.copyTo(desenho);
 		ocupacao = 0;
@@ -92,32 +108,34 @@ int main() {
 			vaga[i] = pegarImagemCortada(src, posicao,
 					Size2f(posVaga[i].largura, posVaga[i].altura), inclinacao);
 			// histograma de cada vaga apenas do Hue do EV
-			Rect a(posicao.x-posVaga[i].largura/2,posicao.y-posVaga[i].altura/2,posVaga[i].largura,posVaga[i].altura);
+			Rect a(posicao.x - posVaga[i].largura / 2,
+					posicao.y - posVaga[i].altura / 2, posVaga[i].largura,
+					posVaga[i].altura);
 			cvtColor(vaga[i], hsv, COLOR_BGR2HSV);
 			split(hsv, channels); // separei os canais H, S e V
 			histograma_h[i] = histCalc(channels[0], 180);
 			// comparacao entre histogramas hue
-			correlacao_h[i] = compareHist(histograma_mapa_h[i],
-					histograma_h[i], HISTCMP_CORREL);
+			correlacao_h[i] = compareHist(histograma_mapa_h[i], histograma_h[i],
+					HISTCMP_CORREL);
 			//cout << "correlacao= " << correlacao_h[i] << " | vaga= " << i+1 << "pos: " << posicao << endl;
 
 			if (i == 1) {
 				//cout << "correlacao= " << correlacao_h[i] << endl;
 			}
-			if (i <= 5){
+			if (i <= 5) {
 				if (correlacao_h[i] > limiarUp) {
-					rectangle(desenho,a,Scalar(0, 255, 0), 2);
+					rectangle(desenho, a, Scalar(0, 255, 0), 2);
 
 				} else {
-					rectangle(desenho,a,Scalar(0, 0, 255), 2);
+					rectangle(desenho, a, Scalar(0, 0, 255), 2);
 					ocupacao++;
 				}
 			} else {
 				if (correlacao_h[i] > limiarDown) {
-					rectangle(desenho,a,Scalar(0, 255, 0), 2);
+					rectangle(desenho, a, Scalar(0, 255, 0), 2);
 
 				} else {
-					rectangle(desenho,a,Scalar(0, 0, 255), 2);
+					rectangle(desenho, a, Scalar(0, 0, 255), 2);
 					ocupacao++;
 				}
 
@@ -125,15 +143,19 @@ int main() {
 
 		}
 		totalVagas = nVagas - ocupacao;
-		namedWindow("Final Result", WINDOW_NORMAL);
-		imshow("Final Result", desenho);
-		waitKey(15);
-		/*if (totalVagas != atual || aux == 0){
-			imwrite("alpha.png", desenho);
-			atual = totalVagas;
+		if (aux == 0) {
+			toc();
 			aux = 1;
-			Whtml(totalVagas);
-		}*/
+		}
+		//namedWindow("Final Result", WINDOW_NORMAL);
+		//imshow("Final Result", desenho);
+		//waitKey(15);
+		/*if (totalVagas != atual || aux == 0){
+		 imwrite("alpha.png", desenho);
+		 atual = totalVagas;
+		 aux = 1;
+		 Whtml(totalVagas);
+		 }*/
 
 	}
 	return 0;
